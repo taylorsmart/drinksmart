64dfa19afaa2dfe84addefaeee5a232e314a949b
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-06-19 18:46:58 -0700

    Update drink-list.jsx

--- client/src/tempData/drink-list.jsx
+++ client/src/tempData/drink-list.jsx
@@ -1,32 +1,45 @@
+const oz_in_g = 29.6;
+
 const drinkMenu = [
+//     {
+//       name:'Mai Tai',
+//       image:'https://www.liquor.com/thmb/bMJ2wMlf0W1QMnmwhS3k_TjV6pc=/735x0/mai-tai-720x720-primary-e09e24f1cacd4b3896f5aa32ba51dcdd.jpg',
+//       description:'Delicious Mai Tai',
+//       recipe:{
+//         'Coca-Cola':18,
+//         'Milk':75
+//       }
+//     },
+//     {
+//       name:'Captains Grogg',
+//       image:'https://augustine-bar.de/wp-content/uploads/Muertos-Y-Furiosos.png',
+//       description:'Sublime Drink for the Shipmaster',
+//       recipe:{
+//         'Tequila':10,
+//         'Orange Juice':25
+//       }
+//     },
+//     {
+//       name:'Pina Colada',
+//       image:'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2012%2F01%2F10%2F4077901-pinacolada-sdw-2000.jpg',
+//       description:'Who doesnt like a Pina Colada?',
+//       recipe:{
+//         'Tequila':10,
+//         'Water':1500,
+//         'Milk was a bad choice':25
+//       }
+//     },
+    
     {
-      name:'Mai Tai',
-      image:'https://www.liquor.com/thmb/bMJ2wMlf0W1QMnmwhS3k_TjV6pc=/735x0/mai-tai-720x720-primary-e09e24f1cacd4b3896f5aa32ba51dcdd.jpg',
-      description:'Delicious Mai Tai',
-      recipe:{
-        'Coca-Cola':18,
-        'Milk':75
-      }
-    },
-    {
-      name:'Captains Grogg',
-      image:'https://augustine-bar.de/wp-content/uploads/Muertos-Y-Furiosos.png',
-      description:'Sublime Drink for the Shipmaster',
-      recipe:{
-        'Tequila':10,
-        'Orange Juice':25
-      }
-    },
-    {
-      name:'Pina Colada',
-      image:'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2012%2F01%2F10%2F4077901-pinacolada-sdw-2000.jpg',
-      description:'Who doesnt like a Pina Colada?',
-      recipe:{
-        'Tequila':10,
-        'Water':1500,
-        'Milk was a bad choice':25
-      }
-    },
+        name: 'Barbados Rum Punch',
+        image: 'https://augustine-bar.de/wp-content/uploads/Muertos-Y-Furiosos.png',
+        description: 'A great drink!',
+        recipe: {
+            'Fresh Lime Juice': 1 * oz_in_g,
+            'Demerara Syrup': 1 * oz_in_g,
+            'Blended Aged Rum - Barbados': 2 * oz_in_g,
+            'Angostura Bitters': 1,
+        }
   ]
 
 export default drinkMenu
